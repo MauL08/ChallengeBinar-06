4ec23e805bfbee0e265c52368fe3f6771a06b86a
MosQyy <akbarma2002@gmail.com>
2022-05-12 12:14:55 +0700

    fix qr code bug

--- src/screens/scan_screen/index.js
+++ src/screens/scan_screen/index.js
@@ -1,5 +1,5 @@
-import { View, Text, Alert } from 'react-native';
-import React from 'react';
+import { View, Text, Alert, TouchableOpacity, Linking } from 'react-native';
+import React, { useState } from 'react';
 
 import { styles } from './styles';
 import { CameraScreen, CameraType } from 'react-native-camera-kit';
@@ -7,22 +7,49 @@ import { useIsFocused } from '@react-navigation/native';
 
 const ScanScreen = () => {
   const focus = useIsFocused();
+  const [scan, setScan] = useState('');
 
-  const onRead = data => {
-    Alert.alert(data.nativeEvent.codeStringValue);
+  const onScan = result => {
+    if (result.length > 0) {
+      Alert.alert('QR Code Result', result, [
+        {
+          text: 'Open URL',
+          onPress: () => {
+            Linking.openURL(result);
+            setScan('');
+          },
+        },
+        {
+          text: 'Cancel',
+          style: 'cancel',
+          onPress: () => setScan(''),
+        },
+      ]);
+    }
+    if (result === '') {
+      setScan('');
+      Alert.alert('Error', 'QR Code not detected.');
+    }
   };
 
   return focus ? (
     <View style={styles.container}>
       <Text style={styles.title}>Scan QR Code</Text>
-      <CameraScreen
-        cameraType={CameraType.Back}
-        scanBarcode={true}
-        onReadCode={e => onRead(e)}
-        showFrame={true}
-        laserColor="#60d67a"
-        frameColor="white"
-      />
+      <View style={styles.cameraContainer}>
+        <CameraScreen
+          cameraType={CameraType.Back}
+          scanBarcode={true}
+          onReadCode={e => {
+            setScan(e.nativeEvent.codeStringValue);
+          }}
+          showFrame={true}
+          laserColor="#60d67a"
+          frameColor="white"
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => onScan(scan)}>
+        <Text style={styles.buttonText}>Capture QR Code</Text>
+      </TouchableOpacity>
     </View>
   ) : null;
 };
--- src/screens/scan_screen/styles.js
+++ src/screens/scan_screen/styles.js
@@ -1,14 +1,35 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import { ms } from 'react-native-size-matters';
 import AppConfig from '../../core/utils/app_config';
 
+const { width } = Dimensions.get('screen');
+
 export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  cameraContainer: {
+    width: width,
+  },
   title: {
     // position: 'absolute',
+    width: width,
     backgroundColor: AppConfig.baseColor,
     textAlign: 'center',
-    paddingBottom: AppConfig.paddingM,
+    paddingVertical: AppConfig.paddingL,
     fontSize: AppConfig.heading2Size,
     fontWeight: 'bold',
     // margin: AppConfig.padding2XL,
+  },
+  button: {
+    padding: AppConfig.paddingXL,
+    borderRadius: ms(8),
+    marginTop: ms(550),
+    backgroundColor: AppConfig.buttonColor,
+  },
+  buttonText: {
+    fontSize: AppConfig.heading3Size,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
