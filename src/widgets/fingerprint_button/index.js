a93c26f44fb99ae96e8d64518ff85533b84d53a4
MosQyy <akbarma2002@gmail.com>
2022-05-12 11:27:24 +0700

    add fingerprint

--- src/widgets/fingerprint_button/index.js
+++ src/widgets/fingerprint_button/index.js
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import TouchID from 'react-native-touch-id';
 
 import { styles } from './styles';
 import { FingerprintIcon } from '../../core/assets';
@@ -8,11 +9,48 @@ import { FingerprintIcon } from '../../core/assets';
 const FingerprintButton = () => {
   const navigation = useNavigation();
 
+  const optionalConfigObject = {
+    title: 'Place your Finger to Scan',
+    imageColor: 'green', // Android
+    imageErrorColor: '#ff0000', // Android
+    sensorDescription: 'Touch Sensor', // Android
+    sensorErrorDescription: 'Failed', // Android
+    cancelText: 'Cancel', // Android
+  };
+
+  const checkHandle = () => {
+    TouchID.isSupported(optionalConfigObject)
+      .then(biometryType => {
+        // Success code
+        if (biometryType === 'FaceID') {
+          console.log('FaceID is supported.');
+        } else {
+          console.log('TouchID is supported.');
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const pressHandler = () => {
+    TouchID.authenticate('', optionalConfigObject)
+      .then(() => {
+        navigation.navigate('Main');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.hint}>Fingerprint Login</Text>
       <TouchableOpacity
-        onPress={() => navigation.navigate('Main')}
+        onPress={() => {
+          checkHandle();
+          pressHandler();
+        }}
         style={styles.logoContainer}>
         <Image source={FingerprintIcon} style={styles.fingerLogo} />
       </TouchableOpacity>
