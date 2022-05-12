65f8ec1488c08a489a17c3015f3a064a47d6df05
MosQyy <akbarma2002@gmail.com>
2022-05-12 19:09:34 +0700

    add maps

--- src/screens/home_screen/index.js
+++ src/screens/home_screen/index.js
@@ -1,21 +1,104 @@
-import { Text, View, Image, TouchableOpacity } from 'react-native';
-import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  PermissionsAndroid,
+} from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigation } from '@react-navigation/native';
+
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import GeoLocation from 'react-native-geolocation-service';
+import Snackbar from 'react-native-snackbar';
 
 import { ExitIcon, LocationIcon, RefreshIcon } from '../../core/assets';
 import { styles } from './styles';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+
+  const [permission, setPermission] = useState(false);
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  const openSnackbar = () => {
+    Snackbar.show({
+      text: 'Location Refreshed',
+      duration: Snackbar.LENGTH_LONG,
+    });
+  };
+
+  const LocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Access Required',
+          message: 'This App needs to Access your location',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        setPermission(granted);
+      } else {
+        setPermission(false);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getLocation = useCallback(() => {
+    if (permission) {
+      GeoLocation.getCurrentPosition(position => {
+        const { longitude, latitude } = position.coords;
+        setLocation({
+          ...location,
+          latitude,
+          longitude,
+        });
+      });
+    }
+  }, [location, permission]);
+
+  useEffect(() => {
+    LocationPermission();
+    getLocation();
+  }, [getLocation, location, permission]);
+
   return (
     <View style={styles.container}>
+      <View style={styles.mapContainer}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: -6.33756,
+            longitude: 106.89662,
+            latitudeDelta: 0.009,
+            longitudeDelta: 0.009,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        </MapView>
+      </View>
       <View style={styles.userContainer}>
         <View>
-          <Text style={styles.greetText}>Hallo, Akbar!</Text>
+          <Text style={styles.greetText}>Hello, Akbar!</Text>
           <View style={styles.locationContainer}>
             <Image source={LocationIcon} style={styles.locationIcon} />
             <Text style={styles.locationText}>Jalan Bambu Hijau</Text>
-            <TouchableOpacity onPress={() => console.log('Location refreshed')}>
+            <TouchableOpacity
+              onPress={() => {
+                openSnackbar();
+                getLocation();
+              }}>
               <Image source={RefreshIcon} style={styles.refreshIcon} />
             </TouchableOpacity>
           </View>
--- src/screens/home_screen/styles.js
+++ src/screens/home_screen/styles.js
@@ -1,17 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { ms } from 'react-native-size-matters';
 import AppConfig from '../../core/utils/app_config';
+
+const { height } = Dimensions.get('screen');
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: AppConfig.primaryColor,
   },
+  mapContainer: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
   userContainer: {
+    elevation: ms(8),
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    margin: AppConfig.padding2XL,
+    marginTop: height - ms(220),
+    marginHorizontal: AppConfig.padding2XL,
     padding: AppConfig.paddingXL,
     backgroundColor: AppConfig.baseColor,
     borderRadius: ms(8),
@@ -24,8 +34,8 @@ export const styles = StyleSheet.create({
   },
   locationIcon: {
     marginRight: AppConfig.paddingXS,
-    height: ms(18),
-    width: ms(14),
+    height: ms(11),
+    width: ms(9),
   },
   locationText: {
     fontSize: AppConfig.normalFontSize,
